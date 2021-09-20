3a7dea271a8623f6430f0433119ece4c67574199
gabrielpino26 <Gab.dev.gm@gmail.com>
2021-09-20 10:03:22 -0500

    test project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# orderbook
+# Orderbook
+An Orderbook app built with React/Typescript and powered by WebSockets.
+
+yarn install
+yarn start
---
+++ src/components/OrderBook/styles.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  min-height: 31.25em;
+  flex-direction: column-reverse;
+  justify-content: center;
+  align-items: center;
+  border-color: #263946;
+
+  @media only screen and (min-width: 800px) {
+    flex-direction: row;
+    justify-content: center;
+  }
+`
+
+interface TableContainerProps {
+  isBids: boolean;
+}
+
+export const TableContainer = styled.div<TableContainerProps>`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  color: #bfc1c8;
+
+  @media only screen and (min-width: 800px) {
+    width: 50%;
+  }
+`
